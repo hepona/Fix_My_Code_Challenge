d60484a2a09c0c206d78e2955bf0eb5d79149c39
hepona <hepona10@gmail.com>
2023-12-09 23:08:41 +0100

    square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
+"""
+square class
+"""
 
 
 class square:
+    """attribute"""
+
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """init"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -14,9 +20,11 @@ class square:
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """permiter of square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """str"""
         return "{}/{}".format(self.width, self.height)
 
 
