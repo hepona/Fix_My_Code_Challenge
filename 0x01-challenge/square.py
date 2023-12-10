56f986ba4e6c018afea84d5833791a964f8d4014
hepona <hepona10@gmail.com>
2023-12-10 01:55:02 +0100

    square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -4,7 +4,7 @@ square class
 """
 
 
-class square:
+class Square:
     """attribute"""
 
     def __init__(self, width, height):
@@ -26,7 +26,7 @@ class square:
 
 
 if __name__ == "__main__":
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.perimeter_of_square())
