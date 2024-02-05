bcd59b5c1fe788965609764f86d434d022b4b021
hepona <hepona10@gmail.com>
2024-02-05 14:11:29 +0100

    sqr

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -7,26 +7,30 @@ square class
 class Square:
     """attribute"""
 
-    def __init__(self, width, height):
+    def __init__(self, size=0):
         """init"""
-        self.width = width
-        self.height = height
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.size = size
 
     def area_of_my_square(self):
         """Area of the square"""
-        return self.width * self.height
+        return self.size * self.size
 
     def perimeter_of_my_square(self):
         """permiter of square"""
-        return self.width * 4
+        return self.size * 4
 
     def __str__(self):
         """str"""
-        return "{}/{}".format(self.width, self.height)
+        return "{}".format(self.size)
 
 
 if __name__ == "__main__":
-    s = Square(width=12, height=8)
+    s = Square(size=12)
     print(s)
     print(s.area_of_my_square())
     print(s.perimeter_of_my_square())
