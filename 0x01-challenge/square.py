4b732c32e0112a494c046cb1261312bead490bdb
hepona <hepona10@gmail.com>
2024-02-05 15:36:27 +0100

    sqrrr

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -6,27 +6,27 @@ square class
 
 class Square():
     """attribute"""
+    side = 0
 
-    def __init__(self, width, height):
+    def __init__(self, side):
         """init"""
-        self.width = width
-        self.height = height
+        self.side = side
 
     def area_of_my_square(self):
         """Area of the square"""
-        return self.width * self.height
+        return self.side**2
 
     def perimeter_of_my_square(self):
         """permiter of square"""
-        return (self.width * 2) + (self.height * 2)
+        return self.side * 4
 
     def __str__(self):
         """str"""
-        return "{}/{}".format(self.width, self.height)
+        return "{}".format(self.side)
 
 
 if __name__ == "__main__":
-    s = Square(width=12, height=9)
+    s = Square(side=12)
     print(s)
     print(s.area_of_my_square())
     print(s.perimeter_of_my_square())
